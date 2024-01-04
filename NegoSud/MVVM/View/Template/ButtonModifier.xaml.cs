c91fa7aa58859510867124c757e8b21582d2e7b9
jlegendre <juliette.legendre@tessi.fr>
2024-01-04 22:59:37 +0100

    Ajout nouveau dossier dans View : Page, Template, Uc       Ajout uc dans le dossier Template

---
+++ NegoSud/MVVM/View/Template/ButtonModifier.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NegoSud.MVVM.View.Template
+{
+    /// <summary>
+    /// Logique d'interaction pour ButtonModifier.xaml
+    /// </summary>
+    public partial class ButtonModifier : UserControl
+    {
+        public ButtonModifier()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonPencil_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
