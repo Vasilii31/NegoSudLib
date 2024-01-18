f96bc8c0fd658f4e02303b85b682bf67ff7482b4
jlegendre <juliette.legendre@tessi.fr>
2024-01-18 16:03:46 +0100

    ajout Page domaines

---
+++ NegoSud/MVVM/View/Page/Domaines.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NegoSud.MVVM.View.Page
+{
+    /// <summary>
+    /// Logique d'interaction pour Domaines.xaml
+    /// </summary>
+    public partial class Domaines : UserControl
+    {
+        public Domaines()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonAjouter_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonAjouter_Loaded_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonAjouter_Loaded_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
