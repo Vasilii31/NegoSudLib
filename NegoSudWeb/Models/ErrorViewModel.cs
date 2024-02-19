f42d16cfad427c920559ac2274cb6606a5f005c4
ChrisBlc <christopherblanc64@gmail.com>
2024-02-19 14:16:32 +0100

    Ajout projet Web

---
+++ NegoSudWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NegoSudWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
